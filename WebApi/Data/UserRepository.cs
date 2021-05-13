bdd0c6ec7bb00d90510d45139e4241ae7049ffe4
GuillemPallares <guillempallares@outlook.es>
2021-05-13 21:07:20 +0200

    feat: Added UserRepository.

---
+++ WebApi/Data/UserRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApi.Models;
+
+namespace WebApi.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private ApplicationDbContext _context;
+
+        public UserRepository()
+        {
+            _context = HttpContext.Current.GetOwinContext().Get<ApplicationDbContext>();
+        }
+
+        public void AddUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicationUser GetUser(string userName)
+        {
+            return _context.Users.FirstOrDefault(u => u.UserName == userName).Obfuscate() ?? throw new KeyNotFoundException();
+        }
+
+        public void TransferToUser(string senderUserName, string recieverUserName, int amount)
+        {
+            var senderUser = _context.Users.FirstOrDefault(u => u.UserName == senderUserName);
+            if ((senderUser.Balance - amount) < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var recieverUser = _context.Users.FirstOrDefault(u => u.UserName == recieverUserName);
+
+            senderUser.Balance -= amount;
+            recieverUser.Balance += amount;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+    }
+
+    public static class ApplicationUserExtensions
+    {
+        public static ApplicationUser Obfuscate(this ApplicationUser user)
+        {
+            if(user != null)
+            {
+                user.PasswordHash = "****";
+                user.SecurityStamp = "****";
+            }
+
+            return user;
+        }
+
+    }
+}
---
+++ WebApi/Models/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public interface IUserRepository
+    {
+        void AddUser();
+        ApplicationUser GetUser(string userName);
+        void TransferToUser(string senderUserName, string recieverUserName, int amount);
+    }
+}
