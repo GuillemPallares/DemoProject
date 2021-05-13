dd4465f7ce60b43617657f8343f7fcc4665b5e63
GuillemPallares <guillempallares@outlook.es>
2021-05-14 00:53:20 +0200

    chore: Added Register and Login Models.

--- WebApi/Controllers/AdminController.cs
+++ WebApi/Controllers/AdminController.cs
@@ -206,11 +206,5 @@ namespace WebApi.Controllers
         }
 
     }
-    public class RegisterModel
-    {
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public int InitialBalance { get; set; }
-    }
+
 }
---
+++ WebApi/Models/LoginModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "User")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Grant")]
+        public string Grant_Type { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Client")]
+        public string Client_Id { get; set; }
+
+    }
+}
