5dd17b9e31cc23ba1b5984535027cdc5b44f5e89
GuillemPallares <guillempallares@outlook.es>
2021-05-14 00:15:21 +0200

    feat: Added UserRepository.

--- WebApi/Data/UserRepository.cs
+++ WebApi/Data/UserRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using WebApi.Models;
 
@@ -10,20 +11,93 @@ namespace WebApi.Data
     public class UserRepository : IUserRepository
     {
         private ApplicationDbContext _context;
+        private ApplicationUserManager _userManager;
+
 
         public UserRepository()
         {
             _context = HttpContext.Current.GetOwinContext().Get<ApplicationDbContext>();
+            _userManager = HttpContext.Current.GetOwinContext().Get<ApplicationUserManager>();
         }
 
-        public void AddUser()
+        public int AddBalance(string userName, int amount)
         {
-            throw new NotImplementedException();
+            var user = FindUserByName(userName);
+
+            user.Balance += amount;
+
+            _context.SaveChanges();
+
+            return user.Balance;
+        }
+
+
+        public async Task<ApplicationUser> AddUserAsync(string userName, string email, string password = "Prueba1234$", int initialBalance = 0)
+        {
+            if(userName == null || email == null) throw new ArgumentNullException("UserName and Email Can't be null.");
+
+            var user = new ApplicationUser()
+            {
+                UserName = userName,
+                Email = email,
+                EmailConfirmed = true,
+                Balance = initialBalance
+            };
+
+            var result = await _userManager.CreateAsync(user, password);
+            if (result.Succeeded)
+            {
+                return user.Obfuscate();
+            }
+
+            throw new Exception(result.Errors.ToString());
+        }
+
+        public void DeleteUser(string userName)
+        {
+            var user = FindUserByName(userName);
+
+            _context.Users.Remove(user);
+
+            _context.SaveChanges();
+
+        }
+
+        public ApplicationUser EditUser(ApplicationUser user)
+        {
+            if(user == null) throw new ArgumentNullException("User cannot be null.");
+
+            var userToEdit = FindUserByName(user.UserName);
+
+            userToEdit.Update(user);
+
+            _context.SaveChanges();
+
+            return userToEdit.Obfuscate();
+        }
+
+        public List<ApplicationUser> GetAllUsers()
+        {
+            return _context.Users.ToList().ObfuscateList();
         }
 
         public ApplicationUser GetUser(string userName)
         {
-            return _context.Users.FirstOrDefault(u => u.UserName == userName).Obfuscate() ?? throw new KeyNotFoundException();
+            return FindUserByName(userName).Obfuscate();
+        }
+
+
+        public int RemoveBalance(string userName, int amount)
+        {
+            var user = FindUserByName(userName);
+
+            if (user.Balance < amount) throw new ArgumentOutOfRangeException("The balance is less than the amount to extract");
+
+            user.Balance -= amount;
+
+            _context.SaveChanges();
+
+            return user.Balance;
         }
 
         public void TransferToUser(string senderUserName, string recieverUserName, int amount)
@@ -50,6 +124,12 @@ namespace WebApi.Data
         }
 
 
+        private ApplicationUser FindUserByName(string userName)
+        {
+            return _context.Users.FirstOrDefault(u => u.UserName == userName) ?? throw new KeyNotFoundException();
+        }
+
+
     }
 
     public static class ApplicationUserExtensions
@@ -60,6 +140,43 @@ namespace WebApi.Data
             {
                 user.PasswordHash = "****";
                 user.SecurityStamp = "****";
+            }
+
+            return user;
+        }
+        
+        public static List<ApplicationUser> ObfuscateList(this List<ApplicationUser> users)
+        {
+            var userobs = new List<ApplicationUser>();
+
+            if(users != null)
+            {
+                foreach (var user in users)
+                {
+                    userobs.Add(user.Obfuscate());
+                }
+            }
+
+            return users;
+        }
+
+        /// <summary>
+        /// With this method we limit the information of the user that can be updated.
+        /// </summary>
+        /// <param name="user">User to update.</param>
+        /// <param name="update">User with the information used to update</param>
+        /// <returns></returns>
+        public static ApplicationUser Update(this ApplicationUser user, ApplicationUser update)
+        {
+            if(user != null)
+            {
+                user.Balance = update.Balance;
+                user.Email = update.Email;
+                user.EmailConfirmed = update.EmailConfirmed;
+                user.PhoneNumber = update.PhoneNumber;
+                user.PhoneNumberConfirmed = update.PhoneNumberConfirmed;
+                user.LockoutEnabled = update.LockoutEnabled;
+                user.LockoutEndDateUtc = update.LockoutEndDateUtc;
             }
 
             return user;
--- WebApi/Models/IUserRepository.cs
+++ WebApi/Models/IUserRepository.cs
@@ -8,8 +8,64 @@ namespace WebApi.Models
 {
     public interface IUserRepository
     {
-        void AddUser();
+        /// <summary>
+        /// Adds a new user.
+        /// </summary>
+        /// <param name="userName">Unique user name to assign</param>
+        /// <param name="email">User`s email</param>
+        /// <param name="password">User's password to asign, if null assigns default</param>
+        /// <param name="initialBalance">User's intial balance, if null defaults to 0</param>
+        Task<ApplicationUser> AddUserAsync(string userName, string email, string password, int initialBalance);
+
+        /// <summary>
+        /// Edits a user.
+        /// </summary>
+        /// <param name="user">User with the new data.</param>
+        /// <returns>Return the edited User</returns>
+        ApplicationUser EditUser(ApplicationUser user);
+        
+        /// <summary>
+        /// Delete the User with the corresponding UserName
+        /// </summary>
+        /// <param name="userName">The User unique Name</param>
+        void DeleteUser(string userName);
+
+        /// <summary>
+        /// Returns a list of all Users.
+        /// </summary>
+        /// <returns></returns>
+        List<ApplicationUser> GetAllUsers();
+
+        /// <summary>
+        /// Adds amount to the balance an returns the new value.
+        /// </summary>
+        /// <param name="userName">User to add the amount</param>
+        /// <param name="amount">Quantity to add.</param>
+        /// <returns>The new balance</returns>
+        int AddBalance(string userName, int amount);
+
+        /// <summary>
+        /// Substracs amount to the balance an returns the new value.
+        /// </summary>
+        /// <param name="userName">User to add the amount</param>
+        /// <param name="amount">Quantity to substract.</param>
+        /// <returns>The new balance</returns>
+        int RemoveBalance(string userName, int amount);
+
+        /// <summary>
+        /// Gets the User by Id.
+        /// </summary>
+        /// <param name="userName">User to retrieve</param>
+        /// <returns>The result User.</returns>
         ApplicationUser GetUser(string userName);
+
+        /// <summary>
+        /// Tranfers the requested amount of money from senderUser to recieverUser.
+        /// </summary>
+        /// <param name="senderUserName">The sender</param>
+        /// <param name="recieverUserName">The reciever</param>
+        /// <param name="amount">Amount to transfer</param>
         void TransferToUser(string senderUserName, string recieverUserName, int amount);
+        
     }
 }
